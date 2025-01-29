7a1ba15c1bf953198e64063dbe953e17624f41d7
waqas1343 <126809168+waqas1343@users.noreply.github.com>
2025-01-29 20:44:32 +0500

    Update multi_providers.dart

--- lib/view_model/providers/multi_providers.dart
+++ lib/view_model/providers/multi_providers.dart
@@ -6,6 +6,7 @@ import '../controller/course_select_controller/course_select_scontroller.dart';
 import '../controller/image_picker/image_picker_controller.dart';
 
 final List<ChangeNotifierProvider> multiAppProviders = [
+  
   ChangeNotifierProvider<ListPractice>(create: (_) => ListPractice()),
   ChangeNotifierProvider<GenderProvider>(create: (_) => GenderProvider()),
   ChangeNotifierProvider<ImagePickerController>(
